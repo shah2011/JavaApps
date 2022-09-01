7a63a5e12d56b1c5af695f0f5477b34d20245363
SM <SM@SMs-MacBook-Pro.lan>
2022-09-01 10:07:17 -0400

    updated assign 3

---
+++ src/Assignment3/SortStringAlpha.java
@@ -0,0 +1,31 @@
+// sort array alphabetically without using sort method:
+
+package Assignment3;
+
+public class SortStringAlpha {
+
+	public static void main(String[] args) {		
+
+		String str = "sachin";
+
+		char arr[] = str.toCharArray();
+
+		char temp;
+
+		for(int i=0; i<arr.length; i++)
+		{
+			for(int j=i+1; j<arr.length; j++)
+			{
+				if(arr[i] > arr[j])
+				{
+					temp = arr[i]; 
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		System.out.println(new String(arr));
+
+	}
+
+}
